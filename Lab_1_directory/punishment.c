ad2eb2d9f2e6ca1073557d7dfb5b432462daba3b
aarav1109s <aarav1109s@gmail.com>
2026-02-06 15:06:36 -0800

    Complete punishment.c

--- Lab_1_directory/punishment.c
+++ Lab_1_directory/punishment.c
@@ -1,46 +1,46 @@
 #include <stdio.h>
 
-// int main(void)
-// {
-//     int x;
-//     int number;
 
-//     printf("Enter the repetition count for the punishment phrase: ");
-
-//     x = scanf("%d", number);
-
-//     if (scanf("%d", number) == 1 && number > 0) {
-
-//     }
-//     else {
-//         int c;
-//         c == getchar
-//     }
-
-//     return 0;
-// }
 int main()
 {
     int count;
     int line;
     int typo;
-    printf("Enter the repition count for the punishment phrase: ");
+    printf("Enter the repetition count for the punishment phrase: ");
     while (1)
     {
         if ((scanf("%d", &count) == 1) && count > 0)
         {
-            // Start the next set here
-            printf("Enter the line where you want to insert the typo: ");
-            scanf("%d", typo);
+            printf("\nEnter the line where you want to insert the typo: ");
+            while(1) {
+                // Start the next set here
+                
+                if((scanf("%d", &typo)) == 1 && typo > 0 && typo <= count) {
+                    printf("\n");
+                    for (int i = 1; i <= count; i++) {
+                        if (i == typo) {
+                            printf("Cading wiht is C avesone!\n");
+                        }
+                        else {
+                            printf("Coding with C is awesome!\n");
+                        }
+                    }
+                    return 0;
+                }
+                else {
+                    int f;
+                    printf("You entered an invalid value for the typo placement! Please re-enter: ");
+                    while ((f = getchar()) != '\n' && f != EOF);
+                    
+                }
+            }
         }
         else
         {
             int c;
-            while ((c = getchar()) != '\n' && c != EOF)
-            {
-                // gets the next char in buffer and it removes it
-                printf("You entered an invalid value for the repetition count! Please re-enter: ");
-            }
+            printf("You entered an invalid value for the repetition count! Please re-enter: ");
+            while ((c = getchar()) != '\n' && c != EOF);
+            
         }
     }
 }
