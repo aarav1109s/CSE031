3f6ccfef9443e3582771b2fe7b396de374c1734e
aarav1109s <aarav1109s@gmail.com>
2026-02-12 16:36:14 -0800

    Final Commit

--- Lab_1_directory/averages.c
+++ Lab_1_directory/averages.c
@@ -1,56 +1,87 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+
 
 int main() {
 
-    int count = 1;
-    int numbers;
-    int odd = 0;
-    int even = 0;
-    int even_count = 0;
-    int odd_count = 0;
+
+   int count = 1;
+   int numbers;
+   double odd = 0;
+   double even = 0;
+   int even_count = 0;
+   int odd_count = 0;
 
 
-    printf("Enter the 1st value: ");
-    
-    while ((scanf("%d", &numbers)) == 1 && numbers != 0) {
-        int sum = 0;
-        if (numbers == 0) {
-            break;
-        }
-        count++;
-        if (count == 2) {
-            printf("Enter the 2nd value: ");
-        }
-        else if (count == 3) {
-            printf("Enter the 3rd value: ");
-        }
-        else {
-            printf("Enter the %dth value: ", count);
-        }
-        int temp = numbers;
-        while (temp > 0) {
-            sum += temp % 10;
-            temp /= 10;
-        }
 
-        
 
-        if (sum % 2 == 0) {
-            even += numbers;
-            even_count++;
-        }
-        else {
-            odd += numbers;
-            odd_count++;
-        }
-    }
+   printf("Enter the 1st value: ");
+  
+   while ((scanf("%d", &numbers)) == 1 && numbers != 0) {
+       int sum = 0;
+  
+       count++;
+       if (count % 100 >= 11 && count % 100 <= 13)
+           printf("Enter the %dth value: ", count);
+       else if (count % 10 == 1)
+           printf("Enter the %dst value: ", count);
+       else if (count % 10 == 2)
+           printf("Enter the %dnd value: ", count);
+       else if (count % 10 == 3)
+           printf("Enter the %drd value: ", count);
+       else
+           printf("Enter the %dth value: ", count);
+       int temp = abs(numbers);
+       while (temp > 0) {
+           sum += temp % 10;
+           temp /= 10;
+       }
 
-    even /= even_count;
-    odd /= odd_count;
 
-    printf("Average of input values whose digits sum up to an even number: %d\n", even);
-    printf("Average of input values whose digits sum up to an odd number: %d\n", odd);
-    
+      
 
-    return 0;
+
+       if (sum % 2 == 0) {
+           even += numbers;
+           even_count++;
+       }
+       else {
+           odd += numbers;
+           odd_count++;
+       }
+   }
+
+
+   if (even_count == 0 && odd_count == 0) {
+       printf("There is no average to compute.\n");
+       return 0;
+   }
+
+
+
+
+   if(even_count != 0) {
+       even /= even_count;
+   }
+   if (odd_count != 0) {
+       odd /= odd_count;
+   }
+
+
+
+
+   if (even_count > 0)
+       printf("Average of input values whose digits sum up to an even number: %.2f\n", even);
+
+
+   if (odd_count > 0)
+       printf("Average of input values whose digits sum up to an odd number: %.2f\n", odd);
+
+
+  
+
+
+   return 0;
 }
